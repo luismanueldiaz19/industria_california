4dddbd588d5e42a53f980fd10247b5b709baca9f
Lwader Soft <luismanueldiaz19@outlook.es>
2026-09-10 10:57:11 -0400

    gg

--- frontend/lib/core/constants.dart
+++ frontend/lib/core/constants.dart
@@ -1,8 +1,8 @@
 // final String host = "http://127.0.0.1:8000";
 
-final String host = "http://192.168.100.7:8080";
+// final String host = "http://192.168.100.7:8080";
 
-/// final String host = "https://neo-proyect.site";
+final String host = "https://lwadersoft.site/";
 
 final logoPath = "assets/logos/logo_california.png";
 
